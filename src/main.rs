026906c18beb5ff12b759010705708ebf6895516
Jon Jensen <jon@endpoint.com>
2021-02-28 08:47:09 -0700

    Use bigger numbers, check bounds, get range from command argument

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-// from https://www.softax.pl/blog/rust-lang-in-a-nutshell-3-traits-and-generics/
+// adapted from https://www.softax.pl/blog/rust-lang-in-a-nutshell-3-traits-and-generics/
 
-struct Fibonacci (u32,u32);
+use std::env;
+
+struct Fibonacci(u128, u128);
 
 impl Iterator for Fibonacci {
-	type Item = u32; //associated Iterator type defined as u32
+    type Item = u128;
 
-	fn next(&mut self) -> Option<u32> {
-		let new_next = self.0 + self.1;
-		self.0 = self.1;
-		self.1 = new_next;
-		Some(self.0)
-	}
+    fn next(&mut self) -> Option<u128> {
+        let new_next: u128 = self.0 + self.1;
+        self.0 = self.1;
+        self.1 = new_next;
+        Some(self.0)
+    }
 }
 
-fn main(){
-    let seq = Fibonacci(0,1);
-    for i in seq.take(10) {
-        print!("{}, ", i);
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Please supply one argument, the Fibonacci sequence number to go to");
+    }
+    let num = args[1].parse().unwrap();
+    let seq = Fibonacci(0, 1);
+    let mut i: u32 = 0;
+    for n in seq.take(num) {
+        i += 1;
+        print!("{}: {}\n", i, n);
     }
 }
